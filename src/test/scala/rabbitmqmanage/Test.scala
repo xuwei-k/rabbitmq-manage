cd178a3772440e9001bf0ccefba335930ed054aa
xuwei-k <6b656e6a69@gmail.com>
2015-02-26 16:02:01 +0900

    debug print

--- src/test/scala/rabbitmqmanage/Test.scala
+++ src/test/scala/rabbitmqmanage/Test.scala
@@ -5,7 +5,10 @@ import httpz._, native._
 object Test {
   def main(args: Array[String]): Unit = {
     val request = httpz.Request(url = "http://localhost:15672/api/overview").auth("guest", "guest")
-    val json = httpz.Core.string(request).map(play.api.libs.json.Json.parse).map(_.as[Overview])
+    val json = httpz.Core.string(request).map(play.api.libs.json.Json.parse).map{ json =>
+      println(json)
+      json.as[Overview]
+    }
     println(json.interpret)
   }
 }

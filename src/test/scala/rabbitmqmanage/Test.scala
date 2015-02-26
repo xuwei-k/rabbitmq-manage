2fbda79c6bec4795b9ff31444c579872b5b954ea
xuwei-k <6b656e6a69@gmail.com>
2015-02-26 16:11:25 +0900

    call some API

--- src/test/scala/rabbitmqmanage/Test.scala
+++ src/test/scala/rabbitmqmanage/Test.scala
@@ -1,12 +1,43 @@
 package rabbitmqmanage
 
 import httpz._, native._
+import play.api.libs.json.Json
 
 object Test {
   def main(args: Array[String]): Unit = {
+    val apis = (
+      "overview" ::
+      "cluster-name" ::
+      "nodes" ::
+      "extensions" ::
+      "definitions" ::
+      "connections" ::
+      "channels" ::
+      "exchanges" ::
+      "queues" ::
+      "bindings" ::
+      "vhosts" ::
+      "users" ::
+      "whoami" ::
+      "permissions" ::
+      "parameters" ::
+      "policies" ::
+      Nil
+    )
+
+    apis.foreach { api =>
+      val request = httpz.Request(url = s"http://localhost:15672/api/$api").auth("guest", "guest")
+      val json = httpz.Core.string(request).map(play.api.libs.json.Json.parse).map { j =>
+        println(Json.prettyPrint(j))
+      }
+      println(json.interpret)
+    }
+  }
+
+  def overview() = {
     val request = httpz.Request(url = "http://localhost:15672/api/overview").auth("guest", "guest")
     val json = httpz.Core.string(request).map(play.api.libs.json.Json.parse).map{ json =>
-      println(json)
+      println(Json.prettyPrint(json))
       json.as[Overview]
     }
     println(json.interpret)

0c253c22360bb621cb8af82017df009c27cbc169
xuwei-k <6b656e6a69@gmail.com>
2015-02-26 16:41:24 +0900

    exchanges

---
+++ src/main/scala/rabbitmqmanage/exchanges/Exchange.scala
@@ -0,0 +1,30 @@
+package rabbitmqmanage
+package exchanges
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsObject, OFormat, __}
+import rabbitmqmanage.exchanges.Exchange.Arguments
+
+final case class Exchange(
+  arguments  :Arguments,
+  autoDelete :Boolean,
+  durable    :Boolean,
+  internal   :Boolean,
+  name       :String,
+  tpe        :String,
+  vhost      :String
+) extends JsonToString[Exchange]
+
+object Exchange {
+  type Arguments = JsObject
+
+  implicit val foramt: OFormat[Exchange] = (
+    (__ \ "arguments").format[Arguments] and
+    (__ \ "auto_delete").format[Boolean] and
+    (__ \ "durable").format[Boolean] and
+    (__ \ "internal").format[Boolean] and
+    (__ \ "name").format[String] and
+    (__ \ "type").format[String] and
+    (__ \ "vhost").format[String]
+  )(apply _, Function.unlift(unapply))
+}
--- src/test/scala/rabbitmqmanage/Test.scala
+++ src/test/scala/rabbitmqmanage/Test.scala
@@ -1,10 +1,15 @@
 package rabbitmqmanage
 
 import httpz._, native._
-import play.api.libs.json.Json
+import play.api.libs.json.{Reads, JsValue, Json}
+import rabbitmqmanage.exchanges.Exchange
 
 object Test {
   def main(args: Array[String]): Unit = {
+    test2()
+  }
+
+  def test1() = {
     val apis = (
       "overview" ::
       "cluster-name" ::
@@ -34,16 +39,28 @@ object Test {
         println(Json.prettyPrint(j))
         println("-" * 100)
       }
-      println(json.interpret)
+      json.interpret
     }
   }
 
-  def overview() = {
-    val request = httpz.Request(url = "http://localhost:15672/api/overview").auth("guest", "guest")
-    val json = httpz.Core.string(request).map(play.api.libs.json.Json.parse).map{ json =>
-      println(Json.prettyPrint(json))
-      json.as[Overview]
+  def test2() = {
+    def Reads[A](implicit A: Reads[A]) = A
+    val list = List[(String, Reads[_])](
+      ("overview", Reads[Overview]),
+      ("exchanges", Reads[List[Exchange]])
+    )
+
+    list.foreach { case (api, reads) =>
+      val request = httpz.Request(url = s"http://localhost:15672/api/$api").auth("guest", "guest")
+      httpz.Core.string(request).map(play.api.libs.json.Json.parse).map { json =>
+        println("-" * 100)
+        println(api)
+        println("-" * 100)
+        println(Json.prettyPrint(json))
+        val obj = json.validate(reads)
+        println(obj.isSuccess)
+        obj.recoverTotal(println)
+      }.interpret
     }
-    println(json.interpret)
   }
 }
